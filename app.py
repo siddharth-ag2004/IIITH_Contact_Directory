081f89ffd8beb372695cce11dc23ff878226f3a5
mandavaishnavi <122745245+mandavaishnavi@users.noreply.github.com>
2023-05-06 22:17:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
 
@@ -5,23 +7,24 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+	return render_template('login_faculty.html')
 
 @app.route('/login', methods=['POST'])
 def login():
-    email = request.form['email']
-    password = request.form['password']
+	username = request.form['username']
+	password = request.form['password']
 
-    conn = sqlite3.connect('users.db')
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM administration WHERE username = ? AND password = ?", (email, password))
-    user = cursor.fetchone()
-    conn.close()
+	# Connect to the database and check if the username and email match
+	conn = sqlite3.connect('users.db')
+	c = conn.cursor()
+	c.execute("SELECT * FROM faculty WHERE username = ? AND password = ?", (username, password))
+	result = c.fetchone()
+	conn.close()
 
-    if user:
-        return redirect(url_for('dashboard'))
-    else:
-        return render_template('index.html', error="Invalid email or password")
+	if result is not None:
+		return redirect(url_for('dashboard'))
+	else:
+		return "Invalid username or email"
 
 @app.route('/dashboard')
 def dashboard():
